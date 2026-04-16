241103af6658320be640b0d7b431b98e4a656f79
FathmaIrshad <fathma.irshad@gmail.com>
2026-04-16 06:21:09 +0000

    node version

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,9 +1,7 @@
 // Write your code here
-const NotFound = () => {
-  return (
-    <div>
-      <h1>Page Not Found</h1>
-    </div>
-  )
-}
+const NotFound = () => (
+  <div>
+    <h1>Page Not Found</h1>
+  </div>
+)
 export default NotFound
